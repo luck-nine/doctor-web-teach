7a7b35670a6c360b8dba08291116d3f5ada70aef
luck <1506024313@qq.com>
2022-08-01 18:49:23 +0800

    主页头部设计header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,75 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header>
+    <el-row class="top">
+      <el-col :span="6">
+        <img src="./assets/logo/byyl-logo1.png" class="logoImg" />
+      </el-col>
+      <el-col :span="4" :offset="14">
+        <el-row style="line-height: 70px;">
+          <el-col :span="3">
+            <img src="./assets/images/antOutline-bell.svg" class="messageImg" />
+          </el-col>
+          <el-col :span="3">
+            <img src="./assets/images/profile.jpg" class="profileImg" />
+          </el-col>
+          <el-col :span="16" style="text-align: left">
+            <span class="profileSpan">用户名：张三府</span>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </header>
   <router-view/>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="less" scoped>
+  .input-icon {
+    height: 10px;
+    width: 5px;
+    margin-left: 2px;
+  }
+  .profileSpan {
+    padding-left: 10px;
+  }
 
-nav {
-  padding: 30px;
+  .logoImg {
+    width: 150px;
+    padding-left: 20px;
+  }
 
-  a {
-    font-weight: bold;
+  .messageImg {
+    width: 25px;
+    height: 25px;
+  }
+
+  .profileImg {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+  }
+
+  .top {
+    height: 60px;
+    border: 1px solid #C3C3C3;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
+  }
+  header {
+    padding: 10px;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      // &. 作用到同一个a标签上
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
