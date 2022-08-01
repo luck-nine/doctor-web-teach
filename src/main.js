69571d88e6b7a4afac95d8e246c0fad7702dd43b
luck <1506024313@qq.com>
2022-08-01 11:05:23 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+
+app.use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const port = process.env.port || process.env.npm_config_port || 8088 //端口
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: ['swiper'],
+  devServer: {
+    host: '127.0.0.1',
+    port: port,
+    open: true,
+    proxy: {
+      // detail: https://cli.veujs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]: {
+        target: `http://localhost:8080`,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    },
+  }
 })
